f5f3bc4210513efa92b2a06474da78923758b43e
Matt FitzGerald <mcfitzg02@gmail.com>
2011-11-07 16:33:33 -0500

    Chapter 7 completed

---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+  user.name                   "Matt FitzGerald"
+  user.email                  "mcfitzg02@gmail.com"
+  user.password               "foobar"
+  user.password_confirmation  "foobar"
+end
